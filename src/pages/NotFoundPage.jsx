40d57ff177a0963682af39031fbd672029fbe3fe
defensoriaGSAT <defensoria@gruposatelite.net>
2022-03-17 08:27:19 -0600

    Maquetando

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -5,12 +5,13 @@ import routes from '../helpers/routes'
 export default function NotFoundPage() {
   return (
     <Container>
-      <Row>
-        <Col className='mt-5'>
+      <Row className='mt-5'>
+        <Col md={{span:6, offset: 3}} className='text-center'>
           <img style={{width: '100%'}} src="/img/404-not-found.svg" alt="error 404" />
+        
+          <h2>¿Te has perdido?</h2>
+          <p>Vuelve al <Link to={routes.home}>inicio</Link></p>
         </Col>
-        <h2>¿Te has perdido?</h2>
-        <p>Vuelve al <Link to={routes.home}>inicio</Link></p>
       </Row>
     </Container>
   )
