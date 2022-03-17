161dac35176b27d41e30b5a88dacb6ef1ef18f55
defensoriaGSAT <defensoria@gruposatelite.net>
2022-03-17 12:59:27 -0600

    Home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import routes from '../helpers/routes'
 
 export default function HomePage() {
   return (
-    <div>
-        <h1>HomePage</h1>
-    </div>
+    <Container>
+      <Row className='mt-5'>
+        <Col xs={{span: 12}} md={{span: 6}} className='mb-2'>
+          <h2>Bienbenid@ al Gestor de Tareas</h2>
+          <p>¡Aquí podrás gestionar tus proyectos!</p>
+          <p>
+            Marca tus tareas como terminadas. Agrega, elimina o actuliaza tus tareas.
+          </p>
+          <div>
+            <Link to={routes.login}>Ingresa</Link> o &nbsp;
+            <Button as={Link} to={routes.register}>Crea una cuenta</Button>
+          </div>
+        </Col>
+        <Col>
+          <img
+            className='img-fluid'
+            src='/img/task-manager.svg'
+            alt='gestor-tareas'
+          />
+          <p>Gestiona tu tiempo, mejora tu productividad!!!</p>
+        </Col>
+      </Row>
+    </Container>
   )
 }
